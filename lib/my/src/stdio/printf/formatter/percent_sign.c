f1bf804df6d0458b3d0c0cf151c5db93b77a7fb8
Gabriel Ravier <gabravier@gmail.com>
2020-11-12 15:41:59 +0100

    Added libmy, some test-related files and an elementary .gitignore
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

---
+++ lib/my/src/stdio/printf/formatter/percent_sign.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2020
+** libmy
+** File description:
+** Implements vasprintf_format_percent_sign
+*/
+
+#include "my/internal/printf/formatter.h"
+
+struct my_string *asprintf_format_percent_sign(struct my_string *destination,
+    va_list arguments, struct my_printf_conversion_info *format_info)
+{
+    (void)arguments;
+    (void)format_info;
+    my_string_append_char(destination, '%');
+    return (NULL);
+}
