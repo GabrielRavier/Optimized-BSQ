512c1379f91f4a89f915195ed6c6a8c81b0a8642
Gabriel Ravier <gabravier@gmail.com>
2025-02-13 03:25:40 +0100

    Made run_tests.sh take exe arg and added separate script for 10k
    The code for running my maps testsuite has been moved into a separate
    script specific to 10k maps - that script (run_tests_10000x10000.sh)
    and the original run_tests.sh now take arguments that determine which
    executable they run their tests on (defaulting to the bsq executable
    `make` produces)

--- tests/BSQ/run_tests.sh
+++ tests/BSQ/run_tests.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
+# Invoked as `./run_tests.sh` or `./run_tests.sh /path/to/bsq`
+# When run without arguments, the script assumes that the executable is located at ../../bsq (as relative to the script, not the current working directory)
+EXE_BSQ=${1:-$(dirname "$0")/../../bsq}
+EXE_BSQ=$(realpath "$EXE_BSQ")
+
 # Execute tests from the directory that which contains the script
 cd "$(dirname "$0")"
-
-EXE_BSQ=$(realpath ../../bsq)
 
 for i in mouli_maps/*
 do
@@ -15,32 +18,7 @@ do
     diff -u <("$EXE_BSQ" "$i") "$solved_filename" &
 done
 
-process_single_my_map()
-{
-    # A bit of a cludge but it works, so... ¯\_(ツ)_/¯
-    solved_filename=my_maps_solved/$(basename $1)
-
-    # Make temporary files for both
-    temp_file_input=`mktemp`
-    temp_file_output=`mktemp`
-
-    # Uncompress the files
-    bzip2 -d -c "$1" >"$temp_file_input"
-    bzip2 -d -c "$solved_filename" >"$temp_file_output"
-
-    diff -u <("$EXE_BSQ" "$temp_file_input") "$temp_file_output"
-
-    # Clean up
-    rm "$temp_file_input"
-    rm "$temp_file_output"
-}
-
-for i in my_maps/*.bz2
-do
-    # Use & to parallelize the tests
-    process_single_my_map "$i" &
-done
-
+./run_tests_10000x10000_maps.sh "$EXE_BSQ" &
 
 # Test command line stuff
 test_multiple_args()
