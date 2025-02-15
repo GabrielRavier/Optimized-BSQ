206e13e218da68e91fc0772ab9fb93da92fc461d
Gabriel Ravier <gabravier@gmail.com>
2025-02-15 03:28:49 +0100

    Added some extra tests for small empty maps

--- tests/BSQ/run_tests.sh
+++ tests/BSQ/run_tests.sh
@@ -29,6 +29,32 @@ do_mouli_maps &
 
 ./run_tests_10000x10000_maps.sh "$EXE_BSQ" &
 
+process_single_my_map()
+{
+    # A bit of a cludge but it works, so... ¯\_(ツ)_/¯
+    solved_filename=my_maps_solved/$(basename $1)
+
+    # Make temporary files for both
+    temp_file_input=`mktemp`
+    temp_file_output=`mktemp`
+
+    # Uncompress the files
+    bzip2 -d -c "$1" >"$temp_file_input"
+    bzip2 -d -c "$solved_filename" >"$temp_file_output"
+
+    ( diff -u <("$EXE_BSQ" "$temp_file_input") "$temp_file_output" | head -n30 ) || printf 'Failed for %s (note: above are the first 30 lines of the diff with the expected output)\n' "$1"
+
+    # Clean up
+    rm "$temp_file_input"
+    rm "$temp_file_output"
+}
+
+for i in my_maps/*.bz2
+do
+    # Use & to parallelize the tests
+    process_single_my_map "$i" &
+done
+
 # Test command line stuff
 test_multiple_args()
 {
