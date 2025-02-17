e4ad29b1523f2a6087e6b739e0f8e5f1c1f057e3
Gabriel Ravier <gabravier@gmail.com>
2025-02-17 14:24:26 +0100

    No functional change but added code for another SIMD verify_file
    That one is better but strspn is still faster so we don't use it - the
    resulting executables are still the same as before

--- src/verify_file.c
+++ src/verify_file.c
@@ -8,54 +8,79 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-#define CHECK_ONLY_TWO_CHARS_IN_RANGE_SLOWER_THAN_STRSPN // I've tried pretty hard but it looks like glibc's strspn is just too good
+__attribute__((hot, always_inline))
+static inline bool check_whole_board_is_dots_and_os_except_newlines_no_simd(const struct board_information *board_info)
+{
+    for (size_t i = 0; i < board_info->num_rows; ++i) {
+        char *row = board_info->board + i * board_info->num_cols;
+        if (row[board_info->num_cols - 1] != '\n')
+            return false;
+        if (strspn(row, "o.") != board_info->num_cols - 1)
+            return false;
+    }
+    return true;
+}
 
 __attribute__((hot))
-static bool check_only_two_chars_in_range(char *start, size_t size, char c1, char c2)
+static bool check_whole_board_is_dots_and_os_except_newlines(const struct board_information *board_info)
 {
-#if defined(__SSE4_2__)
-    __m128i mask = _mm_set_epi8(
-        0, 0, 0, 0, 0, 0, 0,  0,
-        0, 0, 0, 0, 0, 0, c2, c1
-    );
-
-    __m128i *start_m128i = (__m128i *)start;
-    __m128i *end_m128i = (__m128i *)(start + size - 16);
-
-    for (; start_m128i < end_m128i; ++start_m128i) {
-        __m128i data = _mm_loadu_si128(start_m128i);
-        uint32_t result = _mm_cmpistri(mask, data, _SIDD_NEGATIVE_POLARITY | _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_ANY | _SIDD_LEAST_SIGNIFICANT);
-        bool carry = _mm_cmpistrc(mask, data, _SIDD_NEGATIVE_POLARITY | _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_ANY | _SIDD_LEAST_SIGNIFICANT);
-        if (carry)
-            return false;
-    }
-
-    size -= (char *)start_m128i - start;
-    start = (char *)start_m128i;
-#elif defined(__SSE2__)
-    __m128i c1_mask = _mm_set1_epi8(c1);
-    __m128i c2_mask = _mm_set1_epi8(c2);
-    __m128i *start_m128i = (__m128i *)start;
-    __m128i *end_m128i = (__m128i *)(start + size - 16);
-
-    for (; start_m128i < end_m128i; ++start_m128i) {
-        __m128i data = _mm_loadu_si128(start_m128i);
-        __m128i c1_result = _mm_cmpeq_epi8(data, c1_mask);
-        __m128i c2_result = _mm_cmpeq_epi8(data, c2_mask);
-        __m128i or_result = _mm_or_si128(c1_result, c2_result);
-        if (_mm_movemask_epi8(or_result) != 0xFFFF)
-            return false;
-    }
-
-    size -= (char *)start_m128i - start;
-    start = (char *)start_m128i;
+    if (board_info->num_cols < 17)
+        return check_whole_board_is_dots_and_os_except_newlines_no_simd(board_info);
+#if !defined(__SSE2__)
+    return check_whole_board_is_dots_and_os_except_newlines_no_simd(board_info);
 #else
-#define CHECK_ONLY_TWO_CHARS_IN_RANGE_SLOWER_THAN_STRSPN
+    size_t i = 0;
+
+#if !defined(__SSE4_1__)
+    const __m128i dots = _mm_set1_epi8('.');
+    const __m128i os = _mm_set1_epi8('o');
+
+    for (; i < (board_info->num_rows * board_info->num_cols) - 16; i += 16) {
+        __m128i row = _mm_loadu_si128((__m128i *)(board_info->board + i));
+        __m128i row_dots = _mm_cmpeq_epi8(row, dots);
+        __m128i row_os = _mm_cmpeq_epi8(row, os);
+        __m128i row_dots_or_os = _mm_or_si128(row_dots, row_os);
+        uint16_t mask = _mm_movemask_epi8(row_dots_or_os);
+        if (mask != 0xffff) {
+            // Make sure the non o/dot characters are newlines, and that they are at the right position
+            size_t non_dot_i_pos = i + __builtin_ctz(~mask);
+            if (board_info->board[non_dot_i_pos] != '\n')
+                return false;
+            if (non_dot_i_pos % board_info->num_cols != board_info->num_cols - 1)
+                return false;
+            i = non_dot_i_pos + 1 - 16;
+        }
+    }
+#else
+    const __m128i cmpistri_mask = _mm_set_epi8(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '.', 'o');
+
+    for (; i < (board_info->num_rows * board_info->num_cols) - 16; i += 16) {
+        __m128i row = _mm_loadu_si128((__m128i *)(board_info->board + i));
+        uint32_t cmpstri_result = _mm_cmpistri(cmpistri_mask, row, _SIDD_NEGATIVE_POLARITY | _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_ANY | _SIDD_LEAST_SIGNIFICANT);
+        bool carry = _mm_cmpistrc(cmpistri_mask, row, _SIDD_NEGATIVE_POLARITY | _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_ANY | _SIDD_LEAST_SIGNIFICANT);
+        if (carry) {
+            // Make sure the non o/dot characters are newlines, and that they are at the right position
+            size_t non_dot_i_pos = i + cmpstri_result;
+            if (board_info->board[non_dot_i_pos] != '\n')
+                return false;
+            if (non_dot_i_pos % board_info->num_cols != board_info->num_cols - 1)
+                return false;
+            i = non_dot_i_pos + 1 - 16;
+        }
+    }
 #endif
-    for (size_t i = 0; i < size; ++i)
-        if (start[i] != c1 && start[i] != c2)
+
+    for (i = (board_info->num_rows * board_info->num_cols) - 16; i < board_info->num_rows * board_info->num_cols; ++i)
+        if (board_info->board[i] != 'o' && board_info->board[i] != '.' &&
+            (board_info->board[i] != '\n' || i % board_info->num_cols != board_info->num_cols - 1))
+               return false;
+
+    for (i = 0; i < board_info->num_rows; ++i)
+        if (board_info->board[i * board_info->num_cols + board_info->num_cols - 1] != '\n')
             return false;
     return true;
+
+#endif
 }
 
 __attribute__((hot))
@@ -77,18 +102,5 @@ bool verify_file(struct loaded_file *file_as_buffer,
     if (expected_size != (file_as_buffer->size - (board_info->board - file_as_buffer->data)))
         return false;
 
-    for (size_t i = 0; i < board_info->num_rows; ++i) {
-        char *row = board_info->board + i * board_info->num_cols;
-        if (row[board_info->num_cols - 1] != '\n')
-            return false;
-#if !defined(CHECK_ONLY_TWO_CHARS_IN_RANGE_SLOWER_THAN_STRSPN)
-        if (!check_only_two_chars_in_range(row, board_info->num_cols - 1, 'o', '.'))
-            return false;
-#else
-        if (strspn(row, "o.") != board_info->num_cols - 1)
-            return false;
-#endif
-    }
-    return true;
+    return check_whole_board_is_dots_and_os_except_newlines_no_simd(board_info); // The SIMD version is still slower than strspn 😭
 }
-
